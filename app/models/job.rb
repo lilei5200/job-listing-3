f3314d90f08cb8056390a06535620aacce76307d
linhai <linhai107@gmail.com>
2017-02-27 16:32:39 +0800

    can not paixu

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,8 +2,9 @@ class Job < ApplicationRecord
      validates :title, presence: true
      validates :wage_upper_bound, presence:true
      validates :wage_lower_bound, numericality: { greater_than: 0}
-
-
+             scope :published, -> { where(is_hidden: false)}
+             scope :recent, -> { order('created_at DESC') }
+      
 
 
        def publish!
